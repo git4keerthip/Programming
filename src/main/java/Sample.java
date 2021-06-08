011eb245c770fa2b3d4cc85f8f4c90a671a6e894
git4Keerthip <keerthipriya.pcr@gmail.com>
2021-06-08 23:16:47 +0530

    triangle problem

--- src/main/java/Sample.java
+++ src/main/java/Sample.java
@@ -4,7 +4,17 @@ public class Sample {
         printTraingleStars(3);
     }
 
-    public static void printTraingleStars(int k){
+    public static void printTraingleStars(int n){
+        int mid = n-1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 2*n-1; j++) {
 
+                if(j>=(mid-i)&&j<=(mid+i))
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println("");
+        }
     }
 }
